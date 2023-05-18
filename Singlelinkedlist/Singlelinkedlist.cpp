f78aca2b6bb446329f94985b1114bb97124cee73
Alan21103 <syahlansaputra03@gmail.com>
2023-05-18 20:36:02 +0700

    Membuat Node previous dan current

--- Singlelinkedlist/Singlelinkedlist.cpp
+++ Singlelinkedlist/Singlelinkedlist.cpp
@@ -21,7 +21,24 @@ void addNode() {
 	nodeBaru->name = nama;
 
 	if (START == NULL || nim <= START->noMhs) {
-		if (START != NULL && nim == START->noMhs) {
+		if (START != NULL && nim == START->noMhs) 
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
 			cout << "NIM sudah ada" << endl;
 			return;
 		}
